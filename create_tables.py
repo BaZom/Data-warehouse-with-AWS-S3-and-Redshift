16c0b7c896d57dcd1399271aac0fbb1f3e8f8185
Basem Abughallya <31888892+BaZom@users.noreply.github.com>
2022-09-25 20:16:43 +0200

    Update create_tables.py

--- create_tables.py
+++ create_tables.py
@@ -4,18 +4,49 @@ from sql_queries import create_table_queries, drop_table_queries
 
 
 def drop_tables(cur, conn):
+    """ iterates on drop statements in sql_quries.py to drop created tables
+           
+    Args:
+        curr (obj): object of curser class
+        conn (obj): connection object
+        
+    Returns:
+        no return values
+    """  
     for query in drop_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def create_tables(cur, conn):
+    """ iterates on insert statements in sql_quries.py to create staging and star schema tables on redshift cluster
+           
+    Args:
+        curr (obj): object of curser class
+        conn (obj): connection object
+        
+    Returns:
+        no return values
+ """   
     for query in create_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def main():
+    """
+    - Establishes connection to the redshift cluster using configtration data in dwh.cfg and gets
+      cursor to it.
+    - drops created tables  
+    - creates staging and star schema tables
+    - Finally, closes the connection.
+           
+    Args:
+        no Args
+        
+    Returns:
+        no return values
+ """  
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
 
