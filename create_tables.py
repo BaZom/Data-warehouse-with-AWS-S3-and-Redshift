da894c6b7e4c54b7fab556d12542eb51864e2da8
Basem Abughallya <31888892+BaZom@users.noreply.github.com>
2022-09-25 20:13:57 +0200

    Update create_tables.py

--- create_tables.py
+++ create_tables.py
@@ -1,60 +1,29 @@
 import configparser
 import psycopg2
-from sql_queries import copy_table_queries, insert_table_queries
+from sql_queries import create_table_queries, drop_table_queries
 
 
-def load_staging_tables(cur, conn):
-    """ iterates on copy statements in sql_quries.py to load data of files in AWS S3 bucket to staging tables in AWS redsihft
-           
-    Args:
-        curr (obj): object of curser class
-        conn (obj): connection object
-        
-    Returns:
-        no return values
-    """    
-    for query in copy_table_queries:
+def drop_tables(cur, conn):
+    for query in drop_table_queries:
         cur.execute(query)
         conn.commit()
 
 
-def insert_tables(cur, conn):
-    """ iterates on insert statements in sql_quries.py to select data from staging tables and insert them into star schema tables
-
-        Args:
-            curr (obj): object of curser class
-            conn (obj): connection object
-
-        Returns:
-            no return values
-     """       
-    for query in insert_table_queries:
+def create_tables(cur, conn):
+    for query in create_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def main():
-    """
-    - Establishes connection to the redshift cluster using configtration data in dwh.cfg and gets
-      cursor to it.
-    - loads data from S3 bucket files into redshift  
-    - inserts loaded data from staging tables into star schema tables
-    - Finally, closes the connection.
-           
-    Args:
-        no Args
-        
-    Returns:
-        no return values
-    """  
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
 
     conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
     cur = conn.cursor()
-    
-    load_staging_tables(cur, conn)
-    insert_tables(cur, conn)
+
+    drop_tables(cur, conn)
+    create_tables(cur, conn)
 
     conn.close()
 
